c636d07dc5e3a35b1ccdf24802e6260c52886652
David <2dsant3@gmail.com>
2022-05-18 11:59:48 -0300

    first commit

---
+++ screens/LoginScreen.js
@@ -0,0 +1,30 @@
+import { useContext, useState } from 'react';
+import { login } from '../util/auth';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+import AuthContent from '../components/Auth/AuthContent';
+import { Alert } from 'react-native';
+import { AuthContext } from '../store/auth-context';
+
+function LoginScreen() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const authCtx = useContext(AuthContext);
+
+  async function loginHandler({ email, password }) {
+    setIsAuthenticating(true);
+    try {
+      const token = await login(email, password);
+      authCtx.authenticate(token);
+    } catch (error) {
+      Alert.alert('Authentication Failed', 'Could not log you in. Please check your credentials.');
+      setIsAuthenticating(false);
+    }
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="login user..." />
+  }
+
+  return <AuthContent isLogin onAuthenticate={loginHandler} />
+}
+
+export default LoginScreen;
